d66b9250bb6a53ff7abfa3be024467e2cf5e1df5
sgrimee <sgrimee@cisco.com>
2021-11-21 18:11:28 +0100

    Support multiple leds per token

--- word_clock_fr/src/lib.rs
+++ word_clock_fr/src/lib.rs
@@ -1,69 +1,98 @@
-// #![allow(dead_code)]
+#[warn(missing_docs)]
+#[derive(Debug, PartialEq)]
+enum Token {
+    Opening,
+    HourTwo,
+    HourFour,
+    HourThree,
+    HourNine,
+    HourOne,
+    HourSeven,
+    HourEight,
+    HourSix,
+    HourFive,
+    HourNoon,
+    HourTen,
+    HourMidnight,
+    HourEleven,
+    HourSeparatorSingular,
+    HourSeparatorPlural,
+    MinuteMinus,
+    MinuteThe,
+    MinuteTen,
+    MinuteAnd,
+    MinuteQuarter,
+    MinuteTwenty,
+    MinuteFive,
+    MinuteHalfMasc,
+    MinuteHalfFem,
+}
 
-// Text lit for each light number
-const LIT_TEXT: [&str; 27] = [
-    "IL", " EST ", "DEUX", "QUATRE", "TROIS", "NEUF", "UNE", "SEPT", "HUIT", "SIX", "CINQ", "MI",
-    "DI", "X", "MINUIT", "ONZE", " HEURE", "S", " MOINS", " LE", " DIX", " ET", " QUART", " VINGT",
-    " CINQ", " DEMI", "E",
-];
+impl Token {
+    fn to_tuple(&self) -> (&str, Vec<u32>) {
+        match self {
+            Token::Opening => ("IL EST", (0u32..=1).collect()),
+            Token::HourTwo => ("DEUX", vec![2]),
+            Token::HourFour => ("QUATRE", vec![3]),
+            Token::HourThree => ("TROIS", vec![4]),
+            Token::HourNine => ("NEUF", vec![5]),
+            Token::HourOne => ("UNE", vec![6]),
+            Token::HourSeven => ("SEPT", vec![7]),
+            Token::HourEight => ("HUIT", vec![8]),
+            Token::HourSix => ("SIX", vec![9]),
+            Token::HourFive => ("CINQ", vec![10]),
+            Token::HourNoon => ("MIDI", vec![11]),
+            Token::HourTen => ("DIX", vec![12]),
+            Token::HourMidnight => ("MINUIT", vec![13]),
+            Token::HourEleven => ("ONZE", vec![14]),
+            Token::HourSeparatorSingular => ("HEURE", vec![15]),
+            Token::HourSeparatorPlural => ("HEURES", vec![16]),
+            Token::MinuteMinus => ("MOINS", vec![17]),
+            Token::MinuteThe => ("LE", vec![18]),
+            Token::MinuteTen => ("DIX", vec![19]),
+            Token::MinuteAnd => ("ET", vec![20]),
+            Token::MinuteQuarter => ("QUART", vec![21]),
+            Token::MinuteTwenty => ("VINGT", vec![22]),
+            Token::MinuteFive => ("CINQ", vec![23]),
+            Token::MinuteHalfMasc => ("DEMI", vec![24]),
+            Token::MinuteHalfFem => ("DEMIE", vec![25]),
+        }
+    }
 
-const OPENING: &[usize] = &[0, 1];
-const HOUR_TWO: &[usize] = &[2];
-const HOUR_FOUR: &[usize] = &[3];
-const HOUR_THREE: &[usize] = &[4];
-const HOUR_NINE: &[usize] = &[5];
-const HOUR_ONE: &[usize] = &[6];
-const HOUR_SEVEN: &[usize] = &[7];
-const HOUR_EIGHT: &[usize] = &[8];
-const HOUR_SIX: &[usize] = &[9];
-const HOUR_FIVE: &[usize] = &[10];
-const HOUR_NOON: &[usize] = &[11, 12];
-const HOUR_TEN: &[usize] = &[12, 13];
-const HOUR_MIDNIGHT: &[usize] = &[14];
-const HOUR_ELEVEN: &[usize] = &[15];
-const HOUR_SEPARATOR_SINGULAR: &[usize] = &[16];
-const HOUR_SEPARATOR_PLURAL: &[usize] = &[16, 17];
-const MINUTE_MINUS: &[usize] = &[18];
-const MINUTE_THE: &[usize] = &[19];
-const MINUTE_TEN: &[usize] = &[20];
-const MINUTE_AND: &[usize] = &[21];
-const MINUTE_QUARTER: &[usize] = &[22];
-const MINUTE_TWENTY: &[usize] = &[23];
-const MINUTE_FIVE: &[usize] = &[24];
-const MINUTE_HALF_SINGULAR: &[usize] = &[25];
-const MINUTE_HALF_PLURAL: &[usize] = &[25, 26];
+    fn to_str(&self) -> &str {
+        self.to_tuple().0
+    }
 
-/// Return a vector with the light numbers that need to be lit to display the given time.
+    fn to_leds(&self) -> Vec<u32> {
+        self.to_tuple().1
+    }
+}
+
+/// Return a vector with the Tokens needed to display time.
 /// Panics if minute >= 60 or hour >=12
-///
-/// # Examples
-/// ```
-/// use word_clock_fr::lights_for_time;
-/// assert_eq!(lights_for_time(true, 1 as u32, 5 as u32), vec![0, 1, 6, 16, 24]);
-/// ```
-pub fn lights_for_time(is_pm: bool, hour: u32, minute: u32) -> Vec<usize> {
+fn tokens_for_time(is_pm: bool, hour: u32, minute: u32) -> Vec<Token> {
     assert!(minute < 60 && hour < 12);
     // round to 5 minutes
     let minute = minute - (minute % 5);
     let minute_words = match minute {
         0 => vec![],
-        5 => MINUTE_FIVE.to_vec(),
-        10 => MINUTE_TEN.to_vec(),
-        15 => [MINUTE_AND, MINUTE_QUARTER].concat(),
-        20 => MINUTE_TWENTY.to_vec(),
-        25 => [MINUTE_TWENTY, MINUTE_FIVE].concat(),
+        5 => vec![Token::MinuteFive],
+        10 => vec![Token::MinuteTen],
+        15 => vec![Token::MinuteAnd, Token::MinuteQuarter],
+        20 => vec![Token::MinuteTwenty],
+        25 => vec![Token::MinuteTwenty, Token::MinuteFive],
         30 => {
             if hour == 0 {
-                [MINUTE_AND, MINUTE_HALF_SINGULAR].concat()
+                vec![Token::MinuteAnd, Token::MinuteHalfMasc]
             } else {
-                [MINUTE_AND, MINUTE_HALF_PLURAL].concat()
+                vec![Token::MinuteAnd, Token::MinuteHalfFem]
             }
         }
-        35 => [MINUTE_MINUS, MINUTE_TWENTY, MINUTE_FIVE].concat(),
-        40 => [MINUTE_MINUS, MINUTE_TWENTY].concat(),
-        45 => [MINUTE_MINUS, MINUTE_THE, MINUTE_QUARTER].concat(),
-        50 => [MINUTE_MINUS, MINUTE_TEN].concat(),
-        55 => [MINUTE_MINUS, MINUTE_FIVE].concat(),
+        35 => vec![Token::MinuteMinus, Token::MinuteTwenty, Token::MinuteFive],
+        40 => vec![Token::MinuteMinus, Token::MinuteTwenty],
+        45 => vec![Token::MinuteMinus, Token::MinuteThe, Token::MinuteQuarter],
+        50 => vec![Token::MinuteMinus, Token::MinuteTen],
+        55 => vec![Token::MinuteMinus, Token::MinuteFive],
         _ => panic!("that value of minute cannot happen"),
     };
 
@@ -80,29 +109,29 @@ pub fn lights_for_time(is_pm: bool, hour: u32, minute: u32) -> Vec<usize> {
     let hour_words = match hour {
         0 => {
             if is_pm {
-                HOUR_NOON.to_vec()
+                vec![Token::HourNoon]
             } else {
-                HOUR_MIDNIGHT.to_vec()
+                vec![Token::HourMidnight]
             }
         }
-        1 => [HOUR_ONE, HOUR_SEPARATOR_SINGULAR].concat(),
-        2 => [HOUR_TWO, HOUR_SEPARATOR_PLURAL].concat(),
-        3 => [HOUR_THREE, HOUR_SEPARATOR_PLURAL].concat(),
-        4 => [HOUR_FOUR, HOUR_SEPARATOR_PLURAL].concat(),
-        5 => [HOUR_FIVE, HOUR_SEPARATOR_PLURAL].concat(),
-        6 => [HOUR_SIX, HOUR_SEPARATOR_PLURAL].concat(),
-        7 => [HOUR_SEVEN, HOUR_SEPARATOR_PLURAL].concat(),
-        8 => [HOUR_EIGHT, HOUR_SEPARATOR_PLURAL].concat(),
-        9 => [HOUR_NINE, HOUR_SEPARATOR_PLURAL].concat(),
-        10 => [HOUR_TEN, HOUR_SEPARATOR_PLURAL].concat(),
-        11 => [HOUR_ELEVEN, HOUR_SEPARATOR_PLURAL].concat(),
+        1 => vec![Token::HourOne, Token::HourSeparatorSingular],
+        2 => vec![Token::HourTwo, Token::HourSeparatorPlural],
+        3 => vec![Token::HourThree, Token::HourSeparatorPlural],
+        4 => vec![Token::HourFour, Token::HourSeparatorPlural],
+        5 => vec![Token::HourFive, Token::HourSeparatorPlural],
+        6 => vec![Token::HourSix, Token::HourSeparatorPlural],
+        7 => vec![Token::HourSeven, Token::HourSeparatorPlural],
+        8 => vec![Token::HourEight, Token::HourSeparatorPlural],
+        9 => vec![Token::HourNine, Token::HourSeparatorPlural],
+        10 => vec![Token::HourTen, Token::HourSeparatorPlural],
+        11 => vec![Token::HourEleven, Token::HourSeparatorPlural],
         _ => panic!("hour needs to be between 0 and 11"),
     };
 
-    let mut lights = OPENING.to_vec();
-    lights.extend(hour_words);
-    lights.extend(minute_words);
-    lights
+    let mut tokens = vec![Token::Opening];
+    tokens.extend(hour_words);
+    tokens.extend(minute_words);
+    tokens
 }
 
 /// Return a string with the given time using words from the clock in correct order
@@ -128,13 +157,45 @@ pub fn lights_for_time(is_pm: bool, hour: u32, minute: u32) -> Vec<usize> {
 /// assert_eq!(string_for_time(false, 11 as u32, 55 as u32), String::from("IL EST MIDI MOINS CINQ"));
 /// ```
 pub fn string_for_time(is_pm: bool, hour: u32, minute: u32) -> String {
-    // get words from the LIGHTS vector and join them
-    lights_for_time(is_pm, hour, minute)
+    tokens_for_time(is_pm, hour, minute)
         .iter()
-        .map(|x| {
-            *(LIT_TEXT
-                .get(*x)
-                .unwrap_or_else(|| panic!("Light index {} out of bounds.", *x)))
-        })
-        .collect::<String>()
+        .map(|x| x.to_str())
+        .collect::<Vec<&str>>()
+        .join(" ")
+}
+
+/// Return list of leds to turn on to display the given time.
+///
+/// # Examples
+/// ```
+/// use word_clock_fr::leds_for_time;
+/// assert_eq!(leds_for_time(true, 1 as u32, 5 as u32), vec![0, 1, 6, 15, 23]);
+/// ```
+pub fn leds_for_time(is_pm: bool, hour: u32, minute: u32) -> Vec<u32> {
+    let mut leds = Vec::new();
+    for token in tokens_for_time(is_pm, hour, minute).iter() {
+        leds.extend(token.to_leds());
+    }
+    leds
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_token_to_tuple() {
+        assert_eq!(Token::Opening.to_tuple(), ("IL EST", vec![0, 1]));
+    }
+
+    #[test]
+    fn test_tokens_for_time() {
+        let expected = vec![
+            Token::Opening,
+            Token::HourOne,
+            Token::HourSeparatorSingular,
+            Token::MinuteFive,
+        ];
+        assert_eq!(tokens_for_time(true, 1 as u32, 5 as u32), expected);
+    }
 }
